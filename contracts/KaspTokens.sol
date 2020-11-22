52fe07487097362b7fd8bddc147b427a70b4f40d
AAA530 <dhairyapatelad@gmail.com>
2020-11-23 00:31:32 +0530

    added approval function

--- contracts/KaspTokens.sol
+++ contracts/KaspTokens.sol
@@ -6,8 +6,11 @@ contract KaspTokens {
     mapping(address => uint256) public balanceOf;
     string public name;
     string public symbol;
+    mapping(address => mapping(address=>uint256)) public allowance;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor(
         uint256 _initial_supply,
@@ -24,11 +27,21 @@ contract KaspTokens {
         public
         returns (bool success)
     {
-        require(_value <= balanceOf[msg.sender], "Not enough balance");
+        require(_value < balanceOf[msg.sender], "Not enough balance");
         balanceOf[msg.sender] -= _value;
         balanceOf[_to] += _value;
 
         emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        require(balanceOf[msg.sender] > _value,"Not enough balance to spend");
+
+        allowance[_spender][msg.sender] = _value;
+
+        emit Approval(msg.sender,_spender,_value);
         return true;
     }
 }
