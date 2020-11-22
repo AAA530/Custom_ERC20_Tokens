bba07c077f8d619b0f8bdc0693cd124b963e404a
AAA530 <dhairyapatelad@gmail.com>
2020-11-22 11:48:19 +0530

    starting to build smart contract

---
+++ contracts/KaspTokens.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.21 <=0.7.0;
+
+contract KaspTokens {
+    uint256 public totalSupply = 0;
+
+    constructor() public {
+        totalSupply = 100000000;
+    }
+}
