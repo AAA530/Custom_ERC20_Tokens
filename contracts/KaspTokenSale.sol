45bedcc9a68b5a117e331db4bc6bfb36d72a5b9d
AAA530 <dhairyapatelad@gmail.com>
2020-11-23 19:47:36 +0530

    added endSale

--- contracts/KaspTokenSale.sol
+++ contracts/KaspTokenSale.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.21 <=0.7.0;
 import "./KaspTokens.sol";
 
 contract KaspTokenSale {
-    address owner;
+    address payable owner;
     KaspTokens public tokenContract;
     uint256 public tokenprice;
     uint256 tokenSold;
@@ -17,11 +17,30 @@ contract KaspTokenSale {
     }
 
     function buyTokens(uint256 _numberOfTokens) public payable {
-        require( msg.value == _numberOfTokens*tokenprice,"Send money accordingly to purchase");
-        require(tokenContract.balanceOf(address(this))>= _numberOfTokens,"Not enough Tokens to Sell");
-        require(tokenContract.transfer(msg.sender,_numberOfTokens));
+        require(
+            msg.value == _numberOfTokens * tokenprice,
+            "Send money accordingly to purchase"
+        );
+        require(
+            tokenContract.balanceOf(address(this)) >= _numberOfTokens,
+            "Not enough Tokens to Sell"
+        );
+        require(tokenContract.transfer(msg.sender, _numberOfTokens));
         tokenSold += _numberOfTokens;
 
         emit Sell(msg.sender, _numberOfTokens);
+    }
+
+    function endSale() public {
+        require(msg.sender == owner, "Only Owner can excute this function");
+        require(
+            tokenContract.transfer(
+                owner,
+                tokenContract.balanceOf(address(this))
+            )
+        );
+
+        selfdestruct(owner);
+
     }
 }
